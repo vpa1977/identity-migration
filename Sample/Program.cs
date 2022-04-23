f67b44e35485eefd9133aae6b77dedea0a2b58c1
Vladimir <vpa1977@gmail.com>
2022-04-23 12:34:42 +1200

    typo

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -21,8 +21,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddOpenId();
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddAuthentication();
 
